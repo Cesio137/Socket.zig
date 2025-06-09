263aea646e8c4f4d412be09e815c3457f85e4a82
Nathan Miguel <nathan.cm77@gmail.com>
2025-06-09 20:36:10 -0300

    fixed recvd socket

--- src/udp.zig
+++ src/udp.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-const c = @cImport(@cInclude("uv.h"));
+const uv = @cImport(@cInclude("uv.h"));
 const utils = @import("utils.zig");
 
 // Client
@@ -7,48 +7,43 @@ pub const Client = struct {
     // Public
     address: []const u8, 
     port: u16,
-    split_send_buf: bool,
-    send_buf_size: u32, 
-    recv_buf_size: u32,
-    send_cb: ?*const fn ([*c]c.uv_udp_send_t, c_int) callconv(.c) void,
-    recv_cb: ?*const fn ([*c]c.uv_udp_t, isize, [*c]const c.uv_buf_t, [*c]const c.struct_sockaddr, c_uint) callconv(.c) void,
+    send_cb: ?*const fn ([*c]uv.uv_udp_send_t, c_int) callconv(.c) void,
+    recv_cb: ?*const fn ([*c]uv.uv_udp_t, isize, [*c]const uv.uv_buf_t, [*c]const uv.struct_sockaddr, c_uint) callconv(.c) void,
     // Private
-    p_send_socket: c.uv_udp_t,
-    p_broadcast_addr: c.sockaddr_in,
-    p_recv_socket: c.uv_udp_t,
-    p_recv_addr: c.sockaddr_in,
+    p_recv_socket: uv.uv_udp_t,
+    p_recv_addr: uv.sockaddr_in,
 
-    pub fn connect(self: *Client, uv_loop: [*c]c.uv_loop_t) i32 {
-        var code = c.uv_udp_init(uv_loop, &self.*.p_send_socket);
+    pub fn connect(self: *Client, uv_loop: [*c]uv.uv_loop_t) i32 {   
+        // Init client recv socket
+        var code = uv.uv_udp_init(uv_loop, &self.*.p_recv_socket);
         if (code != 0) { return code; }
-        code = c.uv_ip4_addr("0.0.0.0", 0, &self.*.p_broadcast_addr);
+        // Set client ipv4 address and random port
+        code = uv.uv_ip4_addr("0.0.0.0", 0, &self.*.p_recv_addr);
         if (code != 0) { return code; }
-        code = c.uv_udp_bind(&self.*.p_send_socket, @ptrCast(&self.*.p_broadcast_addr), c.UV_UDP_REUSEADDR);
+        // Bind udp settings
+        code = uv.uv_udp_bind(&self.*.p_recv_socket, @ptrCast(&self.*.p_recv_addr), uv.UV_UDP_REUSEADDR);
         if (code != 0) { return code; }
-        code = c.uv_udp_set_broadcast(&self.*.p_send_socket, 1);
-        if (code != 0) { return code; }
-        
-        code = c.uv_udp_init(uv_loop, &self.*.p_recv_socket);
-        if (code != 0) { return code; }
-        code = c.uv_ip4_addr("0.0.0.0", 0, &self.*.p_recv_addr);
-        if (code != 0) { return code; }
-        code = c.uv_udp_bind(&self.*.p_recv_socket, @ptrCast(&self.*.p_recv_addr), c.UV_UDP_REUSEADDR);
-        if (code != 0) { return code; }
-        code = c.uv_udp_recv_start(&self.*.p_recv_socket, utils.alloc_buffer, self.*.recv_cb);
+        // Start listening
+        code = uv.uv_udp_recv_start(&self.*.p_recv_socket, utils.alloc_buffer, self.*.recv_cb);
         if (code != 0) { return code; }
 
         return code;
     }
 
     pub fn sendStr(self: *Client, message: []const u8) i32 {
-        const data: []u8 = @constCast(message);
-        const c_data: [*c]u8 = @ptrCast(data);
-        var uv_buf = c.uv_buf_init(c_data, @intCast(message.len));
-        var send_req: c.uv_udp_send_t = undefined;
-        var send_addr: c.sockaddr_in = undefined;
-        var code = c.uv_ip4_addr("127.0.0.1", 3000, &send_addr);
+        // Convert to C buffer
+        const buf = utils.strToCBuffer(message);
+        // Init uv buffer
+        var uv_buf = uv.uv_buf_init(buf, @intCast(message.len));
+        // UV handler and address
+        var send_req: uv.uv_udp_send_t = undefined;
+        var send_addr: uv.sockaddr_in = undefined;
+        // Set server address to send message
+        const cstr_address = utils.strToCString(self.address);
+        var code = uv.uv_ip4_addr(cstr_address, self.port, &send_addr);
         if (code != 0) { return code; }
-        code = c.uv_udp_send(
+        // Send message
+        code = uv.uv_udp_send(
             &send_req,
             &self.*.p_recv_socket,
             &uv_buf,
@@ -60,13 +55,21 @@ pub const Client = struct {
     }
 
     pub fn sendBuffer(self: *Client, buf: []u8) i32 {
-        var uv_buf = c.uv_buf_init(@ptrCast(buf), buf.len);
-        var send_req: c.uv_udp_send_t = undefined;
-        var send_addr: c.sockaddr_in = undefined;
-        _ = c.uv_ip4_addr("127.0.0.1", 3000, &send_addr);
-        const code = c.uv_udp_send(
+        // Convert to C buffer
+        const c_buf = utils.bufToCBuffer(buf);
+        // Init uv buffer
+        var uv_buf = uv.uv_buf_init(c_buf, buf.len);
+        // UV handler and address
+        var send_req: uv.uv_udp_send_t = undefined;
+        var send_addr: uv.sockaddr_in = undefined;
+        // Set server address to send message
+        const cstr_address = utils.strToCString(self.address);
+        var code = uv.uv_ip4_addr(cstr_address, self.port, &send_addr);
+        if (code != 0) { return code; }
+        // Send message
+        code = uv.uv_udp_send(
             &send_req,
-            &self.*.p_send_socket,
+            &self.*.p_recv_socket,
             &uv_buf,
             1,
             @ptrCast(&send_addr),
@@ -80,13 +83,8 @@ pub fn createDefaultClient() Client {
     const client = Client{ 
         .address = "127.0.0.1", 
         .port = 80,
-        .split_send_buf = true,
-        .send_buf_size = 1024,
-        .recv_buf_size = 1024,
         .send_cb = undefined,
         .recv_cb = undefined,
-        .p_send_socket = undefined,
-        .p_broadcast_addr = undefined,
         .p_recv_socket = undefined,
         .p_recv_addr = undefined,
     };
--- src/utils.zig
+++ src/utils.zig
@@ -1,8 +1,7 @@
 const stdlib = @cImport( @cInclude("stdlib.h") );
 const c = @cImport( @cInclude("uv.h") );
 
-pub fn alloc_buffer(handle: [*c]c.uv_handle_t, suggested_size: usize, buf: [*c]c.uv_buf_t) callconv(.C) void {
-    _ = handle;
+pub fn alloc_buffer(_: [*c]c.uv_handle_t, suggested_size: usize, buf: [*c]c.uv_buf_t) callconv(.C) void {
     const malloc = stdlib.malloc(@intCast(suggested_size));
     if (malloc == null) {
         buf.*.base = null;
@@ -20,3 +19,20 @@ pub fn alloc_buffer(handle: [*c]c.uv_handle_t, suggested_size: usize, buf: [*c]c
     //buf.*.base = malloc[0];
     //buf.*.len = @intCast(suggested_size);
 }
+
+pub fn strToCString(str: []const u8) [*c]const u8 {
+    const c_buf: [*c]const u8 = @ptrCast(str);
+    return c_buf;
+}
+
+pub fn strToCBuffer(str: []const u8) [*c]u8 {
+    const data: []u8 = @constCast(str);
+    const c_buf: [*c]u8 = @ptrCast(data);
+    return c_buf;
+}
+
+pub fn bufToCBuffer(buf: []u8) [*c]u8 {
+    const c_buf: [*c]u8 = @ptrCast(buf);
+    return c_buf;
+}
+
